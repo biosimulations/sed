3367d3cba0f5aa72e3b2c3c4c8ef3b2b38b79d24
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-11-20 10:54:52 -0500

    Initial Set of Internal Types

---
+++ bsedic/pbif/tools/__init__.py
@@ -0,0 +1,11 @@
+
+
+standard_types = {
+    'numeric_result': {
+        'columns': 'list[str]',
+        'values': 'list[float]',
+        'n_spacial_dimensions': 'int'
+    },
+    'numeric_results': 'map[numeric_result]',
+    'columns_of_interest': 'list[str]'
+}
