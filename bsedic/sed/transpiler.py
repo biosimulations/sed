5ebc695ca3b9e9f129e7b750faaee9ef68324791
Ryan Spangler <ryan.spangler@gmail.com>
2025-12-18 13:54:53 -0800

    working on data section

--- bsedic/sed/transpiler.py
+++ bsedic/sed/transpiler.py
@@ -1,7 +1,48 @@
-
-
+import csv
 import json
+import pandas as pd
 
+# TODO: need to import the database
+#   containing information about the
+#   different task types and their
+#   available inputs and outputs
+
+# TODO: provide a way to validate the
+#   document directly without needing to
+#   actually run it (???)
+
+
+def load_csv(location, parameters=None):
+    # TODO: deal with parameters (!)
+    df = pd.read_csv(location)
+    return df
+
+
+MEDIA_TYPES = {
+    "http://purl.org/NET/mediatypes/text/csv": load_csv}
+
+
+def load_data(data_config):
+    # TODO: deal with all error handling (!)
+
+    location = data_config['location']
+    format = data_config['format']
+    parameters = data_config.get('parameters')
+    load_data = MEDIA_TYPES[format]
+    data = load_data(location, parameters)
+
+    return data
+
+
+def load_data_section(data_section_config):
+    data = {}
+    for key, config in data_section_config.items():
+        loaded = load_data(config)
+        data[key] = loaded
+
+    import ipdb; ipdb.set_trace()
+
+    return data
 
 def transpile(sed):
     inputs = sed.get('inputs', {})
@@ -17,9 +58,11 @@ def transpile(sed):
     styles = plots.get('styles', {})
     figures = plots.get('figures', {})
 
-    import ipdb; ipdb.set_trace()
-
     document = {}
+
+    data_section = load_data_section(data)
+
+    import ipdb; ipdb.set_trace()
 
     return document
 
