730c30cd8bd4130301f65cb280cf6fff17f40c78
Lucian Smith <lpsmith@uw.edu>
2026-01-26 12:48:15 -0800

    The 'demo_transpiler' copasi/tellurium code already moved to 'python_transpiler_target'; delete from here.

--- sed/transpiler/demo_transpiler.py
+++ sed/transpiler/demo_transpiler.py
@@ -23,12 +23,27 @@ from basico import (
 PROJECT_ROOT_DIR = Path(__file__).resolve().parents[2]
 
 
-if __name__ == "__main__":
-    core = pbest.globals.get_loaded_core()
-    with open(PROJECT_ROOT_DIR / "examples" / "one" / "example.pbg") as f:
-        pbg = json.load(f)
-        composite = Composite(core=core, config={"state": pbg})
-        print(composite.state["comparison_result"])
-        composite.run(1)
+            copasi_df: DataFrame = run_time_course(
+                start_time=0,
+                duration=20,
+                intervals=50,
+                update_model=True,
+                use_sbml_id=True,
+                model=load_model(str(example_one_dir / "example1.xml"))
+            )
+            copasi_out = {}
+            copasi_out["time"] = np.array(copasi_df.index)
+            copasi_out["S1"] = np.array(copasi_df["S1"])
+            copasi_out["S2"] = np.array(copasi_df["S2"])
+            copasi_out = DataFrame(copasi_out)
 
+            csv_df: DataFrame = pd.read_csv(example_one_dir / "experimental_data.csv")
+            print(csv_df)
+            print(copasi_df)
+            print(tellurium_result)
+            
+            copasi_exp_diff = (copasi_out - csv_df)**2
+            tellurium_exp_diff = (tellurium_result - csv_df)**2
+            copasi_tell_diff = (copasi_out - tellurium_result)**2
+            print(copasi_exp_diff, tellurium_exp_diff, copasi_tell_diff)
 
