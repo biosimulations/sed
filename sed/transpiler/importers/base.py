43a098c56079a373b5f2681a5c977481d6612d19
Lucian Smith <lpsmith@uw.edu>
2026-04-20 10:48:30 -0700

    Split document from base.

--- sed/transpiler/importers/base.py
+++ sed/transpiler/importers/base.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from typing import Any
-from sed.transpiler.importers.tasks import load_tasks_section
-from sed.transpiler.importers.outputs import load_outputs_section
 
 import pandas as pd
 
@@ -50,37 +48,3 @@ class Range(SedBase):
             print("Unsaved data when creating Range:", leftovers)
             return True
         return False
-
-class SedDocument():
-    """The document itself."""
-    def __init__(self, config: dict):
-        self.versionStr = config.pop("versionStr", None)
-        self.versionNum = config.pop("versionNum", None)
-        self.constants = config.pop("constants", None)
-        self.tasks = load_tasks_section(config.pop("tasks", None))
-        self.outputs = load_outputs_section(config.pop("outputs", None))
-        self.validate(config)
-
-    def validate(self, leftovers={}):
-        """Validate."""
-        if len(leftovers):
-            print("Unsaved data when creating SEDDocument:", leftovers)
-            return True
-        return False
-
-    def exportToPython(self, context, path):
-        headers = set()
-        python = "# Translation of SED document v" + sed.versionStr + " to python\n\n"
-        python +=  "\n# tasks:" + task_key + "\n"
-        for task in self.tasks:
-            newheaders, newpython = task.exportToPython()
-            headers.update(newheaders)
-            python += newpython
-        python +=  "\n# outputs:" + task_key + "\n"
-        for output in self.outputs:
-            newheaders, newpython = output.exportToPython()
-            headers.update(newheaders)
-            python += newpython
-        return headers, python
-
-
--- sed/transpiler/transpile_to_python.py
+++ sed/transpiler/transpile_to_python.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 from typing import Any
 
-from sed.transpiler.importers.base import SEDDocument
+from sed.transpiler.importers.document import SEDDocument
 
 logger = logging.getLogger(__name__)
 
